0b3b7ab3a962b4e508bbe6a4c77ca57df07896cc
Harley Inbody <hinbody@gmail.com>
2017-03-22 08:49:26 -0400

    Adds arguments for -t and -y to create directories for tomorrow or yesterday

--- date_dir.sh
+++ date_dir.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-dir_name=$(date +"%C%y%m%d")
-mkdir $dir_name
+
+date_format="%C%y%m%d"
+
+if [ -z "$1" ]
+then
+  dir_name=$(date +"$date_format")
+elif [ "$1" == -y ]
+then
+  dir_name=$(date --date="yesterday" +"$date_format")
+elif [ "$1" == -t ]
+then
+  dir_name=$(date --date="tomorrow" +"$date_format")
+else
+  echo "pass -y for yesterday or -t for tomorrow"
+  echo "pass no argument for today"
+  exit
+fi
+
+if [ -n $dir_name ]
+then
+  mkdir $dir_name
+  exit
+fi
